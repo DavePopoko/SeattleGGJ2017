7c3887d23c9b6fd172242a2b8ea3492e69118406
Dave <dpav@popoko.com>
2017-01-21 00:19:49 -0800

    Initial check in of AI base state machine

---
+++ BadRobot/Assets/Scripts/Enemy/ChaseState.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseState : IEnemyState
+
+{
+    private readonly StatePatternEnemy enemy;
+
+    public ChaseState(StatePatternEnemy statePatternEnemy)
+    {
+        enemy = statePatternEnemy;
+    }
+
+    public void UpdateState()
+    {
+        Look();
+        Chase();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+    }
+
+    public void ToPatrolState()
+    {
+    }
+    public void ToScanState()
+    {
+    }
+
+    public void ToAlertState()
+    {
+        Debug.Log("Enter ALERT (scanning)");
+        enemy.currentState = enemy.alertState;
+    }
+
+    public void ToChaseState()
+    {
+        Debug.Log("Can't transition to same state");
+    }
+    public void ToMeleeAttackState()
+    {
+    }
+
+    public void ToRangeAttackState()
+    {
+    }
+
+
+    private void Look()
+    {
+        RaycastHit hit;
+        Vector3 enemyToTarget = (enemy.chaseTarget.position + enemy.offset) - enemy.eyes.transform.position;
+        if (Physics.Raycast(enemy.eyes.transform.position, enemyToTarget, out hit, enemy.sightRange) && hit.collider.CompareTag("Player"))
+        {
+            enemy.chaseTarget = hit.transform;
+
+        }
+        else
+        {
+            ToAlertState();
+        }
+    }
+
+    private void Chase()
+    {
+        enemy.meshRendererFlag.material.color = Color.red;
+        enemy.navMeshAgent.destination = enemy.chaseTarget.position;
+        enemy.navMeshAgent.Resume();
+    }
+}
---
+++ BadRobot/Assets/Scripts/Enemy/MeleeAttackState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackState : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ToMeleeAttackState()
+    {
+        Debug.Log("Can't transition to same state");
+    }
+
+    public void ToRangeAttackState()
+    {
+
+    }
+}
