7e3863ec548e59e63d202dc706a58247cd789cf1
Livio De La Cruz <livio@livio.me>
2017-01-21 21:31:09 -0800

    added claw, and shiny arms

---
+++ BadRobot/Assets/Scripts/ClawHand.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClawHand : MonoBehaviour
+{
+	public Animator myClaw;
+	public string clawParameterName;
+
+	private SteamVR_TrackedObject steamvr;
+
+	void Awake()
+	{
+		steamvr = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	void Start()
+	{
+		
+	}
+		
+	void FixedUpdate()
+	{
+		SteamVR_Controller.Device device = SteamVR_Controller.Input((int)steamvr.index);
+
+		myClaw.SetBool(clawParameterName, device.GetTouch(SteamVR_Controller.ButtonMask.Trigger));
+	}
+		
+	void Update()
+	{
+		
+	}
+}
