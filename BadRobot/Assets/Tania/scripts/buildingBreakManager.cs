be66c78c8f2198a55348ef071bbbb580f41d4447
chinnie <chinniechinchilla@yahoo.com>
2017-01-21 21:14:04 -0800

    Add new city prefabs and breakable components

---
+++ BadRobot/Assets/Tania/scripts/buildingBreakManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingBreakManager : MonoBehaviour {
+
+    public GameObject remains;
+    public Transform spawnPoints;
+
+    void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+
+                Instantiate(remains, spawnPoints.position, spawnPoints.rotation);
+                Destroy(gameObject);
+            }
+        }  
+
+// spawning test using ESC key
+/*    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Instantiate(remains, spawnPoints.position, spawnPoints.rotation);
+            Destroy(gameObject);
+        }
+    } */
+
+}
