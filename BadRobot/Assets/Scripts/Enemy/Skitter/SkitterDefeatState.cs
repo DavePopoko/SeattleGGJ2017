a48c4abc4ff29e2bb9c60a74d2740d6f82f5a2f8
Dave <dpav@popoko.com>
2017-01-22 17:00:23 -0800

    Missing file

---
+++ BadRobot/Assets/Scripts/Enemy/Skitter/SkitterDefeatState.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class SkitterDefeatState : SkitterBaseState, IEnemyState
+{
+    public SkitterDefeatState(StatePatternSkitter statePatternEnemy)
+    {
+        enemy = statePatternEnemy;
+    }
+
+    public void ToDefeatState()
+    {
+        Debug.Log("Can't transition to same state");
+    }
+
+    public override void UpdateState()
+    {
+        if (enemy.enteredstate)
+        {
+            Debug.Log("Entered Defeat State");
+            enemy.animator.SetBool("isRoboMoving", false);
+            enemy.enteredstate = false;
+        }
+        Look();
+        Search();
+    }
+}
